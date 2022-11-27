b8769b57ac55e4875b08fd044657838a84cb5230
Mr.Viktor.dev <Viktor.for.work94@gmail.com>
2022-11-27 11:26:54 +0600

    dr-5 : Small code optimization in Database.php

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -35,269 +35,205 @@ class DatabaseSeeder extends Seeder
 
     public function generateDress()
     {
-        $dress = [
+        $dresses = [
             [
                 'title' => 'Смок',
                 'description' => 'Оборки в платье такой модели начинаются под грудью и заканчиваются выше колена.',
-                'user_id' => 1,
-                'updated_at' => now(),
-                'created_at' => now()
+                'user_id' => 1
             ],
             [
                 'title' => 'Шифт',
                 'description' => 'воё название оно получило в годы молодёжной революции в Америке в конце 50-х годов.',
-                'user_id' => 2,
-                'updated_at' => now(),
-                'created_at' => now()
+                'user_id' => 2
             ],
             [
                 'title' => 'Труба',
                 'description' => 'Платье труба превосходно подойдёт к типу фигуры песочные часы, а также фигуре с тонкой талией.',
-                'user_id' => 3,
-                'updated_at' => now(),
-                'created_at' => now()
+                'user_id' => 3
             ],
             [
                 'title' => 'Шемиз',
                 'description' => 'Платье данного типа берёт свои корни из далёкого средневековья, когда оно ещё не было платьем.',
-                'user_id' => 4,
-                'updated_at' => now(),
-                'created_at' => now()
+                'user_id' => 4
             ],
             [
                 'title' => 'Футляр',
                 'description' => 'Пожалуй, самая незаменимая вещь в гардеробе каждой женщины. Дамы вы понимаете о чём я?!',
-                'user_id' => 5,
-                'updated_at' => now(),
-                'created_at' => now()
+                'user_id' => 5
             ]
         ];
-        Dress::insert($dress);
+
+        $dressData = [
+            'updated_at' => now(),
+            'created_at' => now()
+        ];
+
+        foreach ($dresses as $dress) {
+            $resultDresses = array_merge($dress, $dressData);
+            Dress::insert($resultDresses);
+        }
     }
 
     public function generateCategory()
     {
-        $category = [
+        $categories = [
             [
                 'title' => 'Зимние',
-                'description' => 'Светлая кожа контрастирует с темными волосами и глазами зачастую тоже темными.',
-                'updated_at' => now(),
-                'created_at' => now()
+                'description' => 'Светлая кожа контрастирует с темными волосами и глазами зачастую тоже темными.'
             ],
             [
                 'title' => 'Летние',
-                'description' => 'Несмотря на летнее название цветотип «лето» довольно холодный. У него нет такого яркого контраста.',
-                'updated_at' => now(),
-                'created_at' => now()
+                'description' => 'Несмотря на летнее название цветотип «лето» довольно холодный. У него нет такого яркого контраста.'
             ],
             [
                 'title' => 'Весенние',
-                'description' => 'Во всем облике девушки «весны» чувствуется мягкость и нежность: светлые шатенки и натуральные блондинки.',
-                'updated_at' => now(),
-                'created_at' => now()
+                'description' => 'Во всем облике девушки «весны» чувствуется мягкость и нежность: светлые шатенки и натуральные блондинки.'
             ],
             [
                 'title' => 'Осенние',
-                'description' => 'Яркие краски осени отражаются в этом цветотипе.Глаза яркие – синие, зеленые, серые, шоколадные.',
-                'updated_at' => now(),
-                'created_at' => now()
+                'description' => 'Яркие краски осени отражаются в этом цветотипе.Глаза яркие – синие, зеленые, серые, шоколадные.'
             ],
             [
                 'title' => 'Коктейльные',
-                'description' => 'сли вы собираетесь посетить шумную вечеринку или провести вечер со своей второй половинкой в ресторане.',
-                'updated_at' => now(),
-                'created_at' => now()
+                'description' => 'сли вы собираетесь посетить шумную вечеринку или провести вечер со своей второй половинкой в ресторане.'
             ]
         ];
-        Category::insert($category);
+        $categoryData = [
+            'updated_at' => now(),
+            'created_at' => now()
+        ];
+        foreach ($categories as $category) {
+            $resultCategories = array_merge($category, $categoryData);
+            Category::insert($resultCategories);
+        }
     }
 
     public function generateDressCategory()
     {
-        $dressCategory = [
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'category_id' => Category::get()->random()->category_id
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'category_id' => Category::get()->random()->category_id
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'category_id' => Category::get()->random()->category_id
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'category_id' => Category::get()->random()->category_id
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'category_id' => Category::get()->random()->category_id
-            ]
+        $dressCategories = [
+            ['dress_id' => 1, 'category_id' => 3],
+            ['dress_id' => 2, 'category_id' => 5],
+            ['dress_id' => 3, 'category_id' => 4],
+            ['dress_id' => 4, 'category_id' => 1],
+            ['dress_id' => 5, 'category_id' => 2]
         ];
-        DressCategory::insert($dressCategory);
+        DressCategory::insert($dressCategories);
     }
 
     public function generateUser()
     {
-        $user = [
+        $users = [
             [
                 'name' => 'Анжела',
                 'email' => 'Anjela@gmail.com',
-                'email_verified_at' => now(),
                 'password' => '123qweasdzxc/.e2hdosaud',
                 'remember_token' => 'LV6OF34fD3',
-                'updated_at' => now(),
-                'created_at' => now()
             ],
             [
                 'name' => 'Виктория',
                 'email' => 'Victorya@gmail.com',
-                'email_verified_at' => now(),
                 'password' => '123qweasaadzxc/.e2hdosaud',
                 'remember_token' => 'NOV6OF34fD3',
-                'updated_at' => now(),
-                'created_at' => now()
             ],
             [
                 'name' => 'Валерия',
                 'email' => 'Valeria@gmail.com',
-                'email_verified_at' => now(),
                 'password' => '123qweafxcsdzxc/.e2hdosaud',
                 'remember_token' => 'KIQ6OF34fD3',
-                'updated_at' => now(),
-                'created_at' => now()
             ],
             [
                 'name' => 'Анастасия',
                 'email' => 'Anastasiya@gmail.com',
-                'email_verified_at' => now(),
                 'password' => '123qweasfadzxc/.e2hdosaud',
                 'remember_token' => 'MAL6OF34fD3',
-                'updated_at' => now(),
-                'created_at' => now()
             ],
             [
                 'name' => 'Алтынай',
                 'email' => 'Altinay@gmail.com',
-                'email_verified_at' => now(),
                 'password' => '123qweaplazxc/.e2hdosaud',
                 'remember_token' => 'MUL6OF34fD3',
-                'updated_at' => now(),
-                'created_at' => now()
             ],
         ];
-        User::insert($user);
+
+        $userData = [
+            'updated_at' => now(),
+            'created_at' => now(),
+            'email_verified_at' => now()
+        ];
+
+        foreach ($users as $user) {
+            $resultUsers = array_merge($user, $userData);
+            User::insert($resultUsers);
+        }
     }
 
     public function generateColor()
     {
-        $color = [
+        $colors = [
             ['color' => 'Blue'],
             ['color' => 'Black'],
             ['color' => 'Red'],
             ['color' => 'Purple'],
             ['color' => 'White'],
         ];
-        Color::insert($color);
+        Color::insert($colors);
     }
 
     public function generateDressColor()
     {
-        $dressColor = [
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'color_id' => Color::get()->random()->color_id,
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'color_id' => Color::get()->random()->color_id,
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'color_id' => Color::get()->random()->color_id,
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'color_id' => Color::get()->random()->color_id,
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'color_id' => Color::get()->random()->color_id,
-            ],
+        $dressColors = [
+            ['dress_id' => 1, 'color_id' => 1],
+            ['dress_id' => 2, 'color_id' => 2],
+            ['dress_id' => 3, 'color_id' => 5],
+            ['dress_id' => 4, 'color_id' => 3],
+            ['dress_id' => 5, 'color_id' => 4],
         ];
-        DressColor::insert($dressColor);
+        DressColor::insert($dressColors);
     }
 
     public function generateSize()
     {
-        $size = [
+        $sizes = [
             ['size' => 'S'],
             ['size' => 'M'],
             ['size' => 'L'],
             ['size' => 'XS'],
             ['size' => 'XM']
         ];
-        Size::insert($size);
+        Size::insert($sizes);
     }
 
     public function generateDressSize()
     {
-        $dressSize = [
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'size_id' => Size::get()->random()->size_id
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'size_id' => Size::get()->random()->size_id
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'size_id' => Size::get()->random()->size_id
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'size_id' => Size::get()->random()->size_id
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'size_id' => Size::get()->random()->size_id
-            ],
+        $dressSizes = [
+            ['dress_id' => 1, 'size_id' => 4],
+            ['dress_id' => 2, 'size_id' => 3],
+            ['dress_id' => 3, 'size_id' => 5],
+            ['dress_id' => 4, 'size_id' => 2],
+            ['dress_id' => 5, 'size_id' => 1],
         ];
-        DressSize::insert($dressSize);
+        DressSize::insert($dressSizes);
     }
 
     public function generatePhoto()
     {
-        $photo = [
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'image' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.jpg',
-                'image_small' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.png'
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'image' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.jpg',
-                'image_small' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.png'
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'image' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.jpg',
-                'image_small' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.png'
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'image' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.jpg',
-                'image_small' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.png'
-            ],
-            [
-                'dress_id' => Dress::get()->random()->dress_id,
-                'image' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.jpg',
-                'image_small' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.png'
-            ],
+        $photos = [
+            ['dress_id' => 1],
+            ['dress_id' => 2],
+            ['dress_id' => 3],
+            ['dress_id' => 4],
+            ['dress_id' => 5],
         ];
-        Photo::insert($photo);
+
+        $images = [
+            'image' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.jpg',
+            'image_small' => 'ptB5gjttmsiXEFK5dk8saMnHLOMcFqbWncVDsCx3s.png'
+        ];
+
+        foreach ($photos as $photo) {
+            $resultPhoto = array_merge($photo, $images);
+            Photo::insert($resultPhoto);
+        }
+
     }
 }
