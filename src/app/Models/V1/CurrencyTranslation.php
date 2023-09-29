823ce3324dfc59a72840f8c38ec3ca7cef7d0ebf
Stas Ignatov <stas@belochka.kz>
2023-09-29 23:29:16 +0600

    DB. Currency. Symbol #78 https://github.com/EvolutionInIT/dress_rent_backend/issues/78

--- src/app/Models/V1/CurrencyTranslation.php
+++ src/app/Models/V1/CurrencyTranslation.php
@@ -6,16 +6,10 @@ use App\Models\Traits\V1\HelperTrait;
 use App\Models\Traits\V1\TranslationTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CurrencyTranslation extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
-
-    // Traits
-    use TranslationTrait;
-    use HelperTrait;
+    use HasFactory, TranslationTrait, HelperTrait;
 
     protected $table = 'currency_translation';
     protected $primaryKey = 'currency_translation_id';
