393e81cc411715210f2534708a533b272fc12bfa
Stas Ignatov <stas@belochka.kz>
2023-10-11 17:17:17 +0600

    SEO. sitemap #1 https://github.com/EvolutionInIT/dress-nuxt/issues/1

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
 
         'api1' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            'throttle:120:api',
             RoleAuthorization::class,
             LangMiddleware::class,
             CurrencyMiddleware::class,
