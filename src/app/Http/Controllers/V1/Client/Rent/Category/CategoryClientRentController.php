b77ac3e1ebcc33d61c65e647fdf806ddd5654412
Stas Ignatov <stas@belochka.kz>
2024-03-14 20:41:43 +0500

    SEO. sitemap #1 https://github.com/EvolutionInIT/dress-nuxt/issues/1

--- src/app/Http/Controllers/V1/Client/Rent/Category/CategoryClientRentController.php
+++ src/app/Http/Controllers/V1/Client/Rent/Category/CategoryClientRentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\V1\Client\Rent\Category;
 
+use App\Http\Requests\V1\Client\Rent\Category\ListCategoryRequest;
 use App\Http\Resources\V1\Client\Category\CategoryResourceClient;
 use App\Models\V1\Category;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
@@ -9,14 +10,19 @@ use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 class CategoryClientRentController
 {
     /**
+     * @param ListCategoryRequest $requestData
      * @return AnonymousResourceCollection
      */
-    public function list(): AnonymousResourceCollection
+    public function list(ListCategoryRequest $requestData): AnonymousResourceCollection
     {
+        $requestData = $requestData->validated();
         $categories =
             Category
                 ::select(['category_id', 'dress_id'])
                 ->with('translation:category_id,title,description')
+                ->when($requestData['with_translations'] ?? false, function ($q) {
+                    $q->with('translations');
+                })
                 ->with('photos')
                 ->orderBy('order', 'desc')
                 ->get();
